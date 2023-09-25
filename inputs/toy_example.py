440127f4becfc6b63f87ecd5ad76ac465d433a36
EvitanRelta <shauntanzongzhi@gmail.com>
2023-09-25 13:57:34 +0800

    Refactor toy example's input constants

--- inputs/toy_example.py
+++ inputs/toy_example.py
@@ -1,13 +1,46 @@
-list_lbs = [[-1,-1], [0.8, -2, -2], [0.8, 0, 0], [0,8, 0, 0]]
-list_ubs = [[1, 1], [4.8, 2, 2], [4.8, 2, 2], [4.8, 2, 2]]
+import torch
+from torch import Tensor
+
+L: list[list[float]] = [[-1, -1], [0.8, -2, -2], [0.8, 0, 0], [0, 8, 0, 0]]
+"""Lower limits for neurons. Each list corresponds to the lower limits for a
+network layer (ie. index-0 is the lower limits for each neuron in layer-0, the
+input layer)."""
+
+U: list[list[float]] = [[1, 1], [4.8, 2, 2], [4.8, 2, 2], [4.8, 2, 2]]
+"""Upper limits for neurons. Each list corresponds to the upper limits for a
+network layer (ie. index-0 is the upper limits for each neuron in layer-0, the
+input layer)."""
+
 # constraint Hx(L)+d <= 0, w.r.t output neurons
 # y1-y2-y3 <= 0, -y2 <= 0, -y3 <= 0, 1-1.25y1 <= 0, y1-2 <= 0, y2-2 <= 0, y3-2 <= 0
-H_matrix = [[1, -1, -1], [0, -1, 0], [0, 0, -1], [-1.25, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]]
-d_vector = [0, 0, 0, 1, -2, -2, -2]
+H: Tensor = torch.tensor(
+    [
+        [1, -1, -1],
+        [0, -1, 0],
+        [0, 0, -1],
+        [-1.25, 0, 0],
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+    ]
+)
+"""`H` matrix in the constraint: `Hx(L) + d <= 0`, w.r.t output neurons."""
+
+d: Tensor = torch.tensor([0, 0, 0, 1, -2, -2, -2])
+"""`d` vector in the constraint: `Hx(L) + d <= 0`, w.r.t output neurons."""
+
 
 # constraint Pxi + P_hatxi_hat - p <= 0, w.r.t intermediate unstable neurons and their respective inputs
 # -x7 <= 0, -x8 <= 0, -0.5x4 +x7 -1 <= 0, -0.5x5+x8 -1 <= 0, 2x4+x5-x7-x8 <= 0, -x7-x8-2 <= 0
 # xi is [x4, x5], xi_hat is [x7, x8]
-P_matrix = [[0,0], [0,0], [-0.5, 0], [0, -0.5], [2, 1], [0,0]]
-Phat_matrix = [[-1,0], [0,-1], [1, 0], [0, 1], [-1, -1], [-1,-1]]
-p_vector = [0, 0, 1, 1, 0, 2]
+P: Tensor = torch.tensor([[0, 0], [0, 0], [-0.5, 0], [0, -0.5], [2, 1], [0, 0]])
+"""`P` matrix in the constraint `Pxi + P_hatxi_hat - p <= 0`, w.r.t
+intermediate unstable neurons and their respective inputs."""
+
+P_hat: Tensor = torch.tensor([[-1, 0], [0, -1], [1, 0], [0, 1], [-1, -1], [-1, -1]])
+"""`P_hat` matrix in the constraint `Pxi + P_hatxi_hat - p <= 0`, w.r.t
+intermediate unstable neurons and their respective inputs."""
+
+p: Tensor = torch.tensor([0, 0, 1, 1, 0, 2])
+"""`p` vector in the constraint `Pxi + P_hatxi_hat - p <= 0`, w.r.t
+intermediate unstable neurons and their respective inputs."""
