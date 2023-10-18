9b93ec41716f6eeb6e29139c531f74c808ea989d
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-18 23:08:20 +0800

    Speed up training by tweaking lr & adding lr-scheduler

--- solve.py
+++ solve.py
@@ -1,11 +1,22 @@
 from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from tqdm.autonotebook import tqdm
 
 from modules.Solver import Solver
 
 
-def train(solver: Solver, lr: float = 1e-2, stop_threshold: float = 1e-5, max_epoches: int = 5000):
+def train(solver: Solver, lr: float = 1, stop_threshold: float = 1e-4, max_epoches: int = 10000):
     optimizer = Adam(solver.parameters(), lr)
+    scheduler = ReduceLROnPlateau(
+        optimizer,
+        mode="min",
+        factor=0.3,
+        patience=2,
+        threshold=0.0001,
+        threshold_mode="rel",
+        cooldown=0,
+        min_lr=1e-5,
+    )
     prev_loss = float("inf")
 
     # Create tqdm object
@@ -26,11 +37,13 @@ def train(solver: Solver, lr: float = 1e-2, stop_threshold: float = 1e-5, max_ep
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        scheduler.step(loss.item())
 
         solver.clamp_parameters()
 
         # Set the description for tqdm
         pbar.set_description(f"Epoch {epoch}")
-        pbar.set_postfix({"Loss": -loss.item()})  # Use set_postfix to display the loss
+        current_lr = optimizer.param_groups[0]["lr"]
+        pbar.set_postfix({"Loss": -loss.item(), "LR": current_lr})
 
     return
