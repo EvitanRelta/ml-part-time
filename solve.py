d6a087f1974522b81d8e02be49485ad4e08fac4d
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-23 15:27:09 +0800

    Perform adv-check every 10 epoches instead of at the end

--- solve.py
+++ solve.py
@@ -39,20 +39,12 @@ def solve(
     new_U: list[Tensor] = []
     for layer_index in range(len(solver.layers) - 1):  # Don't solve for last layer
         solver.reset_and_solve_for_layer(layer_index)
-        thetas = train(solver)
+        if not train(solver):
+            return (False, None, None, solver) if return_solver else (False, None, None)
 
         new_L_i, new_U_i = solver.get_updated_bounds(layer_index)
         new_L.append(new_L_i)
         new_U.append(new_U_i)
-
-        L_0: Tensor = solver.vars.layer_vars[0].L_i
-        U_0: Tensor = solver.vars.layer_vars[0].U_i
-        concrete_inputs: Tensor = torch.where(thetas >= 0, L_0, U_0)
-        unique_concrete_inputs = torch.unique(concrete_inputs, dim=0)
-        assert isinstance(unique_concrete_inputs, Tensor)
-
-        if solver.adv_check_model.forward(unique_concrete_inputs):
-            return (False, None, None, solver) if return_solver else (False, None, None)
 
     # Add last initial bounds.
     new_L.append(solver.vars.L[-1])
