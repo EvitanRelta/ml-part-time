d03bd0f9496c9c1c363bf2faadfaa1496c8ccfb1
EvitanRelta <shauntanzongzhi@gmail.com>
2023-11-01 18:08:14 +0800

    Rearrange class definitions

--- modules/solver_layers.py
+++ modules/solver_layers.py
@@ -78,34 +78,6 @@ class SolverInputLayer(SolverLayer):
     # fmt: on
 
 
-class SolverOutputLayer(SolverLayer):
-    @override
-    def __init__(self, vars: OutputLayerVariables) -> None:
-        super().__init__(vars)
-        self.vars: OutputLayerVariables
-        self.gamma: nn.Parameter = nn.Parameter(
-            torch.rand((self.vars.num_batches, self.vars.H.size(0)))
-        )
-
-    @override
-    def forward(self, V_next: Tensor = torch.empty(0)) -> Tensor:
-        # Assign to local variables, so that they can be used w/o `self.` prefix.
-        H, gamma = self.vars.H, self.gamma  # fmt: skip
-
-        V_L = (-H.T @ gamma.T).T
-        assert V_L.dim() == 2
-        return V_L
-
-    @override
-    def clamp_parameters(self) -> None:
-        self.gamma.clamp_(min=0)
-
-    @override
-    def get_obj_sum(self) -> Tensor:
-        device = self.gamma.device
-        return torch.zeros((1,)).to(device)
-
-
 class SolverIntermediateLayer(SolverLayer):
     @override
     def __init__(self, vars: IntermediateLayerVariables) -> None:
@@ -173,3 +145,31 @@ class SolverIntermediateLayer(SolverLayer):
             )
             - pi_i @ p_i
         )
+
+
+class SolverOutputLayer(SolverLayer):
+    @override
+    def __init__(self, vars: OutputLayerVariables) -> None:
+        super().__init__(vars)
+        self.vars: OutputLayerVariables
+        self.gamma: nn.Parameter = nn.Parameter(
+            torch.rand((self.vars.num_batches, self.vars.H.size(0)))
+        )
+
+    @override
+    def forward(self, V_next: Tensor = torch.empty(0)) -> Tensor:
+        # Assign to local variables, so that they can be used w/o `self.` prefix.
+        H, gamma = self.vars.H, self.gamma  # fmt: skip
+
+        V_L = (-H.T @ gamma.T).T
+        assert V_L.dim() == 2
+        return V_L
+
+    @override
+    def clamp_parameters(self) -> None:
+        self.gamma.clamp_(min=0)
+
+    @override
+    def get_obj_sum(self) -> Tensor:
+        device = self.gamma.device
+        return torch.zeros((1,)).to(device)
