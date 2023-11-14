15d65ea45ef98d256ac2c22e9a1b06db8cbef5cc
EvitanRelta <shauntanzongzhi@gmail.com>
2023-11-14 21:26:02 +0800

    Plot Gurobi-comparison for MNIST 256x6 train script

--- train_mnist_256x6.py
+++ train_mnist_256x6.py
@@ -1,10 +1,25 @@
 import torch
 
-from src.inputs.mnist_256x6 import solver_inputs
+from src.compare_against_gurobi import compare_against_gurobi
+from src.inputs.mnist_256x6 import gurobi_results, solver_inputs
 from src.solve import solve
 from src.utils import seed_everything
 
 seed_everything(0)
-solve(solver_inputs, device=torch.device("cpu"))
+is_falsified, new_L, new_U, solver = solve(
+    solver_inputs,
+    device=torch.device("cuda"),
+    return_solver=True,
+)
 
-print("Training finished!")
+unstable_masks = solver.vars.unstable_masks
+
+compare_against_gurobi(
+    new_L=new_L,
+    new_U=new_U,
+    unstable_masks=unstable_masks,
+    initial_L=solver_inputs.L,
+    initial_U=solver_inputs.U,
+    gurobi_results=gurobi_results,
+    cutoff_threshold=1e-5,
+)
