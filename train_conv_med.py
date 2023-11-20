fec218f81381f50dfb9bc7bff90cfe4be15adb50
EvitanRelta <shauntanzongzhi@gmail.com>
2023-11-20 19:04:24 +0800

    Update ConvMed solving script

--- train_conv_med.py
+++ train_conv_med.py
@@ -1,10 +1,29 @@
 import torch
 
-from src.inputs.conv_med import solver_inputs
+from src.compare_against_gurobi import compare_against_gurobi
+from src.inputs.conv_med import gurobi_results, solver_inputs
 from src.solve import solve
 from src.utils import seed_everything
 
 seed_everything(0)
-solve(solver_inputs, device=torch.device("cpu"))
+is_falsified, new_L_list, new_U_list, solver = solve(
+    solver_inputs,
+    device=torch.device("cpu"),
+    return_solver=True,
+)
 
-print("Training finished!")
+if is_falsified:
+    print("Verification problem is falsified.")
+    exit(0)
+
+unstable_masks = solver.vars.unstable_masks
+
+compare_against_gurobi(
+    new_L_list=new_L_list,
+    new_U_list=new_U_list,
+    unstable_masks=unstable_masks,
+    initial_L_list=solver_inputs.L_list,
+    initial_U_list=solver_inputs.U_list,
+    gurobi_results=gurobi_results,
+    cutoff_threshold=1e-5,
+)
